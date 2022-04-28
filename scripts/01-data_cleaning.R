fae010dcabe67de7f598b765b4d74d83335e4e13
Ritvik Puri <ritvikpuri00@gmail.com>
2022-04-27 23:39:03 -0400

    fixed bugs

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,35 +1,10 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the survey data downloaded from https://open.toronto.ca/dataset/daily-shelter-overnight-service-occupancy-capacity/
+# Author: Ritvik Puri
+# Data: 26 Apil 2022
+# Contact: ritvik.puri@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
 
-
-#### Workspace setup ####
-# Use R Projects, not setwd().
-library(haven)
-library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
-
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
-         
-
-#### What's next? ####
 
 
 
